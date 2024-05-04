84555100183696265a36afb0eef5b1f977b8b056
panduputragit <put.rariadi1144@gmail.com>
2024-05-04 17:50:22 +0800

    starter-pack

---
+++ pages/about.tsx
@@ -0,0 +1,15 @@
+"use strict";
+import "../app/globals.css";
+import React from 'react'
+
+const About = () => {
+  return (
+    <>
+        <div className=''>
+            test
+        </div>
+    </>
+  )
+}
+
+export default About
