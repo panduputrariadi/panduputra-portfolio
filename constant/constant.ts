a8b991c2c235733c1e9ff9ea7cc0d07971b979ec
panduputragit <put.rariadi1144@gmail.com>
2024-05-09 18:52:56 +0800

    update-img

--- constant/constant.ts
+++ constant/constant.ts
@@ -36,9 +36,10 @@ export const projects = [
     id: 1,
     title: "Krisna Komputer Sales Information System",
     description: "Description of Project 1",
-    imageUrl: "/images/person-1.png",
+    imageUrl: "/images/p-1.png",
+    href: "https://github.com/panduputrariadi/krisna-komputer.git",
     author: {
-      name: "Morris Muthigani",
+      name: "Pandu Putra",
       imageUrl: "/images/person-1.png"
     },
     publishedDate: "12/01/2024",
@@ -48,9 +49,10 @@ export const projects = [
     id: 2,
     title: "UD Dewi Suter Market Information System",
     description: "Description of Project 2",
-    imageUrl: "/images/person-1.png",
+    imageUrl: "/images/p-2.png",
+    href: "https://github.com/panduputrariadi/uddewisuter",
     author: {
-      name: "John Doe",
+      name: "Pandu Putra",
       imageUrl: "/images/person-1.png"
     },
     publishedDate: "20/03/2024",
@@ -60,9 +62,10 @@ export const projects = [
     id: 3,
     title: "Wisata Desa Sukawati's Static Website",
     description: "Description of Project 2",
-    imageUrl: "/images/person-1.png",
+    imageUrl: "/images/p-3.png",
+    href: "https://github.com/panduputrariadi/wisata-desa-sukawati",
     author: {
-      name: "John Doe",
+      name: "Pandu Putra",
       imageUrl: "/images/person-1.png"
     },
     publishedDate: "20/03/2024",
@@ -72,9 +75,10 @@ export const projects = [
     id: 4,
     title: "Restful API School With Express js",
     description: "Description of Project 2",
-    imageUrl: "/images/person-1.png",
+    imageUrl: "/images/p-4.png",
+    href: "https://github.com/panduputrariadi/API-SCHOOL-WITH-EXPRESS",
     author: {
-      name: "John Doe",
+      name: "Pandu Putra",
       imageUrl: "/images/person-1.png"
     },
     publishedDate: "20/03/2024",
@@ -84,9 +88,10 @@ export const projects = [
     id: 5,
     title: "To Do App With React",
     description: "Description of Project 2",
-    imageUrl: "/images/person-1.png",
+    imageUrl: "/images/p-5.png",
+    href: "https://github.com/panduputrariadi/todoApp",
     author: {
-      name: "John Doe",
+      name: "Pandu Putra",
       imageUrl: "/images/person-1.png"
     },
     publishedDate: "20/03/2024",
@@ -96,9 +101,10 @@ export const projects = [
     id: 6,
     title: "Auto Restore Database with Golang",
     description: "Description of Project 2",
-    imageUrl: "/images/person-1.png",
+    imageUrl: "/images/p-6.png",
+    href: "https://github.com/panduputrariadi/auto_restore_db",
     author: {
-      name: "John Doe",
+      name: "Pandu putra",
       imageUrl: "/images/person-1.png"
     },
     publishedDate: "20/03/2024",
