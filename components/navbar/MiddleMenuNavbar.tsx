df970d5971fef5067c7f50b260a2b25f5847fd2e
panduputragit <put.rariadi1144@gmail.com>
2024-05-13 22:45:10 +0800

    nav link active

--- components/navbar/MiddleMenuNavbar.tsx
+++ components/navbar/MiddleMenuNavbar.tsx
@@ -1,10 +1,10 @@
-import { useRouter } from "next/navigation";
+import { usePathname } from 'next/navigation'
 import { NAV_LINKS } from "@/constant/constant";
 import Link from "next/link";
 import React from "react";
 
 const MiddleMenuNavbar = () => {
-
+  const pathname = usePathname();
 
   return (
     <div className="hidden lg:flex items-center gap-6 font-semibold uppercase">
@@ -12,11 +12,15 @@ const MiddleMenuNavbar = () => {
         <div key={index} className="relative">
           <Link
             href={link.href}
-            className="cursor-pointer transition duration-300 ease-in-out hover:text-blue-600"
+            className={`cursor-pointer transition duration-300 ease-in-out hover:text-blue-600 ${
+              pathname === link.href ? 'text-blue-600' : ''}`
+            }
           >
             {link.label}
           </Link>
-          <span className="absolute left-0 bottom-0 w-full h-px transform origin-left scale-x-0 transition-all duration-300"></span>
+          {pathname === link.href && (
+            <span className="absolute left-0 bottom-0 w-full h-px transform origin-left scale-x-100 transition-all duration-300 bg-blue-600"></span>
+          )}
         </div>
       ))}
     </div>
--- components/navbar/SideBarMenu.tsx
+++ components/navbar/SideBarMenu.tsx
@@ -1,16 +1,26 @@
 import { NAV_LINKS, SOCIAL_LINKS } from "@/constant/constant";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 import { FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
 
 const SideBarMenu = () => {
+  const pathname = usePathname();
   return (
     <div>
       {NAV_LINKS.map((link, index) => (
         <div key={index} className="my-10">
-          <Link color="foreground" href={link.href}>
+          <Link
+            href={link.href}
+            className={`cursor-pointer transition duration-300 ease-in-out hover:text-blue-600 ${
+              pathname === link.href ? 'text-blue-600' : ''}`
+            }
+          >
             {link.label}
           </Link>
+          {pathname === link.href && (
+            <span className="absolute left-0 bottom-0 w-full h-px transform origin-left scale-x-100 transition-all duration-300 bg-blue-600"></span>
+          )}
         </div>
       ))}
       <div className="flex justify-start items-center gap-4">
